fb56edd8ff270b50d490e1ff5fde00f5903b40e4
Eli Brody <ewokker@gmail.com>
2012-03-24 23:01:49 +0200

    Initial commit. BTW a good fork candidate for a blank project. Just need to change the names of some files.

---
+++ src/cellularApp.cpp
@@ -0,0 +1,70 @@
+#include "cellularApp.h"
+
+//--------------------------------------------------------------
+void cellularApp::setup()
+{
+}
+
+//--------------------------------------------------------------
+void cellularApp::update()
+{
+
+}
+
+//--------------------------------------------------------------
+void cellularApp::draw()
+{
+	ofBackground(100, 255);
+}
+
+//--------------------------------------------------------------
+void cellularApp::keyPressed  (int key)
+{
+    switch (key){
+        case 'a':
+            //rotateAmount.z -= 0.05f;
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void cellularApp::keyReleased  (int key)
+{
+}
+
+//--------------------------------------------------------------
+void cellularApp::mouseMoved(int x, int y )
+{
+}
+
+//--------------------------------------------------------------
+void cellularApp::mouseDragged(int x, int y, int button)
+{
+
+}
+
+//--------------------------------------------------------------
+void cellularApp::mousePressed(int x, int y, int button)
+{
+}
+
+//--------------------------------------------------------------
+void cellularApp::mouseReleased(int x, int y, int button)
+{
+
+}
+
+//--------------------------------------------------------------
+void cellularApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void cellularApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void cellularApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "ofMain.h"
+#include "cellularApp.h"
+#include "ofAppGlutWindow.h"
+
+//========================================================================
+int main( ){
+
+	ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+
+	//int wid = 1200;
+	//float aspect = 1.77f;
+	//ofSetupOpenGL(&window, wid, (int) (wid / aspect), OF_WINDOW);
+
+	//window.setGlutDisplayString("rgba double samples>=4");
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new cellularApp());
+
+}
